d7d69d0ad32a638d6858f2b6631c8e967f2275bc
jazzhong1 <36650330+jazzhong1@users.noreply.github.com>
2018-08-14 12:54:23 +0900

    eee

---
+++ src/main/java/com/kh/spring/reserve/controller/ReserController.java
@@ -0,0 +1,12 @@
+package com.kh.spring.reserve.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+
+import com.kh.spring.store.controller.StoreController;
+
+@Controller
+public class ReserController {
+	private Logger logger = Logger.getLogger(StoreController.class);
+
+}
