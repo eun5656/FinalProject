e17af733b9beda1fa614c5003afaabf56d3c3041
eun5656 <>
2018-08-20 12:37:17 +0900

    etwt

--- src/main/java/com/kh/spring/bookmark/model/service/BookmarkServiceImpl.java
+++ src/main/java/com/kh/spring/bookmark/model/service/BookmarkServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kh.spring.bookmark.model.service;
 
+import java.util.List;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -7,28 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.spring.bookmark.model.dao.BookmarkDao;
+import com.kh.spring.bookmark.model.vo.Bookmark;
 
 @Service
 public class BookmarkServiceImpl implements BookmarkService {
-	@Autowired
-	private BookmarkDao dao;
+   @Autowired
+   private BookmarkDao dao;
 
-	@Autowired
-	SqlSessionTemplate sqlSession;
+   @Autowired
+   SqlSessionTemplate sqlSession;
 
-	@Override
-	public int insertBookmark(Map map) {
-		int result=dao.insertBookmark(map,sqlSession);
-		return result;
-	}
+   @Override
+   public int insertBookmark(Bookmark bookmark) {
+      int result=dao.insertBookmark(bookmark,sqlSession);
+      return result;
+   }
 
-	@Override
-	public int deleteBookmark(Map map) {
-		int result=dao.deleteBookmark(map,sqlSession);
-		return result;
-	}
+   @Override
+   public int deleteBookmark(Map map) {
+      int result=dao.deleteBookmark(map,sqlSession);
+      return result;
+   }
 
-	
-	
-	
+   @Override
+   public List<Bookmark> selectBookMarkList(int memberPk) {
+      List<Bookmark> list=dao.selectBookMarkList(memberPk,sqlSession);
+      return list;
+   }
+
+   
+   
+   
 }
